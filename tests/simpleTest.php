0523dc328f21a9c115501645495b5574405f2984
nlisgo <nathan@aeopus.com>
2015-09-03 14:13:01 +0100

    extend tests to check HTML abstract and digest

--- tests/simpleTest.php
+++ tests/simpleTest.php
@@ -12,6 +12,7 @@ class simpleTest extends PHPUnit_Framework_TestCase
         $this->temp_folder = 'tests/tmp/';
         $this->bib_folder = 'tests/fixtures/bib/';
         $this->ris_folder = 'tests/fixtures/ris/';
+        $this->html_folder = 'tests/fixtures/html/';
     }
 
     public function testJatsToBibtex()
@@ -22,8 +23,8 @@ class simpleTest extends PHPUnit_Framework_TestCase
         foreach ($cits as $cit) {
             $file = basename($cit);
             $compares[] = [
-              file_get_contents($this->temp_folder . $file),
-              file_get_contents($cit),
+                file_get_contents($cit),
+                file_get_contents($this->temp_folder . $file),
             ];
         }
 
@@ -40,8 +41,8 @@ class simpleTest extends PHPUnit_Framework_TestCase
         foreach ($cits as $cit) {
             $file = basename($cit);
             $compares[] = [
-              file_get_contents($this->temp_folder . $file),
-              file_get_contents($cit),
+                file_get_contents($cit),
+                file_get_contents($this->temp_folder . $file),
             ];
         }
 
@@ -49,4 +50,78 @@ class simpleTest extends PHPUnit_Framework_TestCase
             $this->assertEquals($compare[0], $compare[1]);
         }
     }
+
+    public function testJatsToHtmlAbstract() {
+
+        $compares = $this->compareSection('-abstract', "//*[@id='abstract']");
+
+        foreach ($compares as $compare) {
+            $this->assertEqualHtml($compare[0], $compare[1]);
+        }
+    }
+
+    public function testJatsToHtmlDigest() {
+
+        $compares = $this->compareSection('-digest', "//*[@id='elife-digest']");
+
+        foreach ($compares as $compare) {
+            $this->assertEqualHtml($compare[0], $compare[1]);
+        }
+    }
+
+    /**
+     * Prepare array of actual and expected results.
+     */
+    protected function compareSection($suffix, $xpath_query) {
+        $expected = 'expected';
+        $htmls = glob($this->html_folder . "*" . $suffix . ".html");
+        $compares = [];
+
+        foreach ($htmls as $html) {
+            $file = str_replace($suffix, '', basename($html));
+            $actualDom = new DomDocument();
+            libxml_use_internal_errors(TRUE);
+            $actualDom->loadHTMLFile($this->temp_folder . $file);
+            libxml_clear_errors();
+            $xpath = new DOMXPath($actualDom);
+            $elements = $xpath->query($xpath_query);
+
+            $expectedDom = new DomDocument();
+            $html = file_get_contents($html);
+            $expectedDom->loadXML('<' . $expected . '>' . $html . '</' . $expected . '>');
+
+            $compares[] = [
+                $this->getInnerHtml($expectedDom->getElementsByTagName($expected)->item(0)),
+                $this->getInnerHtml($elements->item(0)),
+            ];
+        }
+
+        return $compares;
+    }
+
+    /**
+     * Compare two HTML fragments.
+     */
+    protected function assertEqualHtml($expected, $actual)
+    {
+        $from = ['/\>[^\S ]+/s', '/[^\S ]+\</s', '/(\s)+/s', '/> </s'];
+        $to = ['>', '<', '\\1', '><'];
+        $this->assertEquals(
+            preg_replace($from, $to, $expected),
+            preg_replace($from, $to, $actual)
+        );
+    }
+
+    /**
+     * Get inner HTML.
+     */
+    function getInnerHtml($node) { 
+        $innerHTML= ''; 
+        $children = $node->childNodes; 
+        foreach ($children as $child) { 
+            $innerHTML .= $child->ownerDocument->saveXML($child); 
+        }
+
+        return trim($innerHTML);
+    } 
 }
